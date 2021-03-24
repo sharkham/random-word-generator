9650d095b12553f723e64645f9c0bbae18ac0d0b
Sam Markham <sam.e.markham@gmail.com>
2021-03-24 15:47:06 -0700

    Add boilerplate layout to Generator

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Generator = ({ dataset }) => {
   return (
     <div>
-      Randomly generated word goes here.
+      <p>Randomly generated word goes here.</p>
+      <button>Generate</button>
     </div>
   );
 }
