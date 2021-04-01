1217c3470900a3370a50a92354dd050a160efad2
Sam Markham <sam.e.markham@gmail.com>
2021-04-01 15:25:00 -0700

    Add generate function to Generator

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Generator = ({ dataset }) => {
+
+  const generate = () => {
+    return dataset[Math.floor(Math.random() * dataset.length)];
+  }
+
   return (
     <div>
       <p>Randomly generated word goes here.</p>
